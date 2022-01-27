37295a7a4c7b5936092aba34a2024a361d2e4f02
Jonathan Lin <jonathanlinonline@gmail.com>
2022-01-27 12:06:02 -0600

    Restructure

--- Theseus/Characters/Player.gd
+++ Theseus/Characters/Player.gd
@@ -1,16 +1,31 @@
-extends Node2D
+extends KinematicBody2D
 
+const SPEED = 75
+var velocity = Vector2()
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_d"):
+		$AnimatedSprite.play("run_right")
+		velocity.x = SPEED
+	
+	if Input.is_action_pressed("ui_a"):
+		$AnimatedSprite.play("run_left")
+		velocity.x = -SPEED
+		
+	elif !Input.is_action_pressed("ui_a") and !Input.is_action_pressed("ui_d"):
+		velocity.x = 0
+		
+	if Input.is_action_pressed("ui_w"):
+		if !Input.is_action_pressed("ui_a") and !Input.is_action_pressed("ui_d"):
+			$AnimatedSprite.play("run_up")
+		velocity.y = -SPEED
+	
+	if Input.is_action_pressed("ui_s"):
+		if !Input.is_action_pressed("ui_a") and !Input.is_action_pressed("ui_d"):
+			$AnimatedSprite.play("run_down")
+		velocity.y = SPEED
+		
+	elif !Input.is_action_pressed("ui_w") and !Input.is_action_pressed("ui_s"):
+		velocity.y = 0
+		
+	velocity = move_and_slide(velocity)
