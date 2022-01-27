ef1c742dfc43b5ff630693aa79adde0e05694ddc
Jonathan Lin <jonathanlinonline@gmail.com>
2022-01-27 00:10:04 -0600

    File Restructure

--- Theseus/Player.gd
+++ Theseus/Player.gd
@@ -5,20 +5,24 @@ var velocity = Vector2()
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_d"):
-		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("run_right")
 		velocity.x = SPEED
 	
 	if Input.is_action_pressed("ui_a"):
-		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("run_left")
 		velocity.x = -SPEED
 		
 	elif !Input.is_action_pressed("ui_a") and !Input.is_action_pressed("ui_d"):
 		velocity.x = 0
 		
 	if Input.is_action_pressed("ui_w"):
+		if !Input.is_action_pressed("ui_a") and !Input.is_action_pressed("ui_d"):
+			$AnimatedSprite.play("run_up")
 		velocity.y = -SPEED
 	
 	if Input.is_action_pressed("ui_s"):
+		if !Input.is_action_pressed("ui_a") and !Input.is_action_pressed("ui_d"):
+			$AnimatedSprite.play("run_down")
 		velocity.y = SPEED
 		
 	elif !Input.is_action_pressed("ui_w") and !Input.is_action_pressed("ui_s"):
