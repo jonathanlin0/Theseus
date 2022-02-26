ada3fb8f7ac9463b40a0ba8175797c798a493461
Jonathan Lin <jonathanlinonline@gmail.com>
2022-02-26 12:38:13 -0600

    Added Credits

--- Theseus/Menu/Title_Screen.gd
+++ Theseus/Menu/Title_Screen.gd
@@ -12,3 +12,19 @@ func _ready():
 
 func _on_Play_pressed():
 	get_tree().change_scene("res://Levels/Level1.tscn")
+
+
+func _on_Controls_pressed():
+	pass # Replace with function body.
+
+
+func _on_TextureButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_Exit_pressed():
+	pass # Replace with function body.
+
+
+func _on_Credits_pressed():
+	pass # Replace with function body.
