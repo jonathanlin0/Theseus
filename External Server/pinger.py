1f2929a983ea637e1396acec98da83d1311b54d8
Jonathan Lin <jonathanlinonline@gmail.com>
2022-03-10 19:58:04 -0600

    Leaderboard

--- External Server/pinger.py
+++ External Server/pinger.py
@@ -5,17 +5,19 @@ from time import sleep
 
 while True:
   
+  try:
+    URL = "https://TheseusLeaderboardServer.jonathanlin04.repl.co"
+    
+    r = requests.get(url = URL)
 
-  URL = "https://TheseusLeaderboardServer.jonathanlin04.repl.co"
-  
-  r = requests.get(url = URL)
+    
+    if "200" not in str(r):
+      print("WEBSITE ERROR")
+      print(r)
+    if "200" in str(r):
+      print("Successful ping to https://TheseusLeaderboardServer.jonathanlin04.repl.co")
 
-  
-  if "200" not in str(r):
-    print("ERROR")
-    print(r)
-  if "200" in str(r):
-    print("Successful ping to https://TheseusLeaderboardServer.jonathanlin04.repl.co")
-
-  print("")
-  sleep(5)
+    print("")
+    sleep(5)
+  except:
+    print("ERROR WITH BOT")
--- Theseus/Misc/Leaderboard.gd
+++ Theseus/Misc/Leaderboard.gd
@@ -6,23 +6,39 @@ func _ready():
 	
 	$HTTPRequest_test.request("https://theseusleaderboardserver.jonathanlin04.repl.co/test")
 	
+	$place_1.text = ""
+	$place_2.text = ""
+	$place_3.text = ""
 	
-	
-	"""
-	var file = File.new()
-	file.open("res://leaderboard.json", File.READ)
-	var text = file.get_as_text()
-	dict = JSON.parse(text)
-	file.close()
-	
-	print(text)
-	"""
 
 # overall function to fill leaderboard with data from internet
 func fill_leaderboard():
-	var sorted_dict = []
+	var sorted_names = []
+	var sorted_scores = []
+	
+	
 
-	print(dict)
+	# this sorts the dictionary by score into sorted_names and sorted_scores
+	for player_name in dict["Leaderboard"]:
+		var inserted = false
+
+		for i in range(0, len(sorted_scores)):
+			if dict["Leaderboard"].get(player_name).get("time_elapsed") < sorted_scores[i]:
+				sorted_scores.insert(i, dict["Leaderboard"].get(player_name).get("time_elapsed"))
+				sorted_names.insert(i, player_name)
+				inserted = true
+		if inserted == false:
+			sorted_scores.push_back(dict["Leaderboard"].get(player_name).get("time_elapsed"))
+			sorted_names.push_back(player_name)
+	
+	print(sorted_names)
+
+	if len(sorted_names) > 0:
+		$place_1.text = "1st: " + sorted_names[0] + " - " + str(sorted_scores[0]) + " points"
+	if len(sorted_names) > 1:
+		$place_2.text = "2nd: " + sorted_names[1] + " - " + str(sorted_scores[1]) + " points"
+	if len(sorted_names) > 2:
+		$place_3.text = "3rd: " + sorted_names[2] + " - " + str(sorted_scores[2]) + " points"
 
 # assign the dict using the local leaderboard file
 func load_local_data():
@@ -42,7 +58,6 @@ func _on_HTTPRequest_test_request_completed(result, response_code, headers, body
 	else:
 		var URL = "https://theseusleaderboardserver.jonathanlin04.repl.co/leaderboard"
 		#var URL = "https://theseusleaderboardserver.jonathanlin04.repl.co/" + master_data.username + "/" + str(master_data.start_time) + "/" + str(master_data.end_time) + "/"
-		print(URL)
 		$HTTPRequest_leaderboard.request(URL)
 
 func _on_HTTPRequest_leaderboard_request_completed(result, response_code, headers, body):
