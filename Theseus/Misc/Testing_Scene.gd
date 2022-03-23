8814e2a4ea9a136a1b4b1385921ed4a110881380
Jonathan Lin <jonathanlinonline@gmail.com>
2022-03-22 22:20:43 -0500

    misc

--- Theseus/Misc/Testing_Scene.gd
+++ Theseus/Misc/Testing_Scene.gd
@@ -9,6 +9,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	master_data.level = 0
 
 func _process(delta):
 	if Input.is_action_pressed("ui_w") and $Damage_Text.visible == true:
