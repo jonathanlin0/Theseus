ca850521adcb70bfa5185e607a8efcb67dd86051
Baconsizzle1738 <zhengdundun@gmail.com>
2022-03-23 08:25:53 -0500

    helth bar

--- Theseus/Bosses/Snake Goddess/Snake_Goddess.gd
+++ Theseus/Bosses/Snake Goddess/Snake_Goddess.gd
@@ -5,14 +5,19 @@ var previous_animation = "statue"
 var spawning = false
 var can_be_damaged = false
 
-var speed = Vector2()
 
+var speed = Vector2()
+var health = master_data.snake_goddess_health
 
 
 func _physics_process(delta):
 	if master_data.hypotnuse(get_parent().get_child(3).get_global_position(), get_global_position())<100 and sleeping:
 		activate()
 	
+
+func damage(dmg):
+	if can_be_damaged:
+		health-=dmg
 
 func activate():
 	sleeping = false
